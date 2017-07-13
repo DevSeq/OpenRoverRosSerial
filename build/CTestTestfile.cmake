d1d103a7a81f1ad4ca7812424db0c49bfb13a8a7
Count Zero <king.seth@gmail.com>
2017-07-12 23:03:28 -0400

    First commit added all the files for workspace.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/countzero/repos/catkin_ws/src
+# Build directory: /home/countzero/repos/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
