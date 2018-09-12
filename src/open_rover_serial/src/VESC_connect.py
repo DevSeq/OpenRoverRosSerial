4ca5dc47c035cf6be56e0429c0d49deceea97a0b
Count Zero <king.seth@gmail.com>
2018-09-11 21:25:05 -0400

    started testing code that will find the VESC controllers.

--- src/open_rover_serial/src/VESC_connect.py
+++ src/open_rover_serial/src/VESC_connect.py
@@ -58,7 +58,7 @@ ALIVE_ID = 30
 REBOOT_ID = 29
 GET_VALUES_ID = 4
 
-
+'''
 def process_buffer(buffer):
     """
     This function processes the given buffer (bytearray). When a packet is received, the function calls the process_packet fucntion
@@ -144,13 +144,61 @@ def get_duty_for_counter(counter):
     return min(0.05 * counter, 0.95)
 
 
+
+
+
+def test():
+    phase = 0
+    payload = bytearray()
+    length = 0
+    crc = 0
+    vesc_usb = serial.Serial('COM3', 38400, timeout=0.1)
+    time.sleep(1)  # give the connection a second to settle
+
+    current_control = Packet(8, CURRENT_CONTROL_ID, NO_SCALE, 32, 0.3, CURRENT_CONTROL_ACCURACY)
+    current_control_high = Packet(8, CURRENT_CONTROL_ID, NO_SCALE, 32, 0.5, CURRENT_CONTROL_ACCURACY)
+    duty_control = Packet(8, DUTY_CONTROL_ID, NO_SCALE, 32, 0.1, DUTY_CONTROL_ACCURACY)
+    alive = Packet(8, ALIVE_ID, NO_SCALE)
+    reboot = Packet(8, REBOOT_ID, NO_SCALE)
+    stop = Packet(8, 6, 1, 32, 0, CURRENT_CONTROL_ACCURACY)
+    get_rt_data = Packet(8, GET_VALUES_ID, NO_SCALE)
+
+    get_rt_data.send(vesc_usb)
+    data = [[] for x in range(20)]
+    counter = 0
+    data_counter = 0
+    wait_until = time.time() + 1
+    data_req_sent = False
+    while True:
+        alive.send(vesc_usb)
+        if process_buffer(vesc_usb.read(10)):
+            data_counter += 1
+            data_req_sent = False
+            if data_counter > 10:
+                counter += 1
+                data_counter = 0
+                Packet(8, DUTY_CONTROL_ID, NO_SCALE, 32, get_duty_for_counter(counter), DUTY_CONTROL_ACCURACY).send(vesc_usb)
+                time.sleep(0.3)
+                alive.send(vesc_usb)
+                time.sleep(0.3)
+            get_rt_data.send(vesc_usb)
+        if counter > 19:
+            break
+    stop.send(vesc_usb)
+    text_file = open("Output.txt", "w")
+    print(data)
+    text_file.writelines("%s\n" % numpy.average(item) for item in data)
+    text_file.close()
+
+'''
+
+
 def findandmapcontrollers():
     result = dict()
     left_key = "left"
     right_key = "right"
     if sys.platform.startswith ('linux'):
-        temp_list = glob.glob ('/dev/tty[A-Za-z]*')
-
+        temp_list = glob.glob('/dev/tty[A]*')
 
         for a_port in temp_list:
 
@@ -163,46 +211,6 @@ def findandmapcontrollers():
 
     return result
 
-
-
-phase = 0
-payload = bytearray()
-length = 0
-crc = 0
-vesc_usb = serial.Serial('COM3', 38400, timeout=0.1)
-time.sleep(1)  # give the connection a second to settle
-
-current_control = Packet(8, CURRENT_CONTROL_ID, NO_SCALE, 32, 0.3, CURRENT_CONTROL_ACCURACY)
-current_control_high = Packet(8, CURRENT_CONTROL_ID, NO_SCALE, 32, 0.5, CURRENT_CONTROL_ACCURACY)
-duty_control = Packet(8, DUTY_CONTROL_ID, NO_SCALE, 32, 0.1, DUTY_CONTROL_ACCURACY)
-alive = Packet(8, ALIVE_ID, NO_SCALE)
-reboot = Packet(8, REBOOT_ID, NO_SCALE)
-stop = Packet(8, 6, 1, 32, 0, CURRENT_CONTROL_ACCURACY)
-get_rt_data = Packet(8, GET_VALUES_ID, NO_SCALE)
-
-get_rt_data.send(vesc_usb)
-data = [[] for x in range(20)]
-counter = 0
-data_counter = 0
-wait_until = time.time() + 1
-data_req_sent = False
-while True:
-    alive.send(vesc_usb)
-    if process_buffer(vesc_usb.read(10)):
-        data_counter += 1
-        data_req_sent = False
-        if data_counter > 10:
-            counter += 1
-            data_counter = 0
-            Packet(8, DUTY_CONTROL_ID, NO_SCALE, 32, get_duty_for_counter(counter), DUTY_CONTROL_ACCURACY).send(vesc_usb)
-            time.sleep(0.3)
-            alive.send(vesc_usb)
-            time.sleep(0.3)
-        get_rt_data.send(vesc_usb)
-    if counter > 19:
-        break
-stop.send(vesc_usb)
-text_file = open("Output.txt", "w")
-print(data)
-text_file.writelines("%s\n" % numpy.average(item) for item in data)
-text_file.close()
+if __name__ == '__main__':
+    print "Test!!"
+    findandmapcontrollers()
