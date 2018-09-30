ba9e6a2f26320a3431b30500263d2f02f7af3187
Count Zero <king.seth@gmail.com>
2018-09-29 22:29:32 -0400

    integrated vesc class into ros script.

--- src/open_rover_serial/src/openroverserial.py
+++ src/open_rover_serial/src/openroverserial.py
@@ -3,43 +3,36 @@
 import roslib; roslib.load_manifest('wiimote')
 import rospy
 from geometry_msgs.msg import Twist
-import serial, time, numpy
-from packets import Packet
-import packets
+from vesc import Vesc
+
+
+
 
 
 K = 0.75 / 2
 scale = 5
-
-left_wheel  = None
-right_wheel = None
 v_left = 0.0
 v_right = 0.0
 
 
 
 def callback(data):
-        global v_left
-        global v_right
-        v_left  = scale * (data.linear.x - K * data.linear.y)
-        v_right = scale * (data.linear.x + K * data.linear.y)
-        print "v_l {0} v_r {1} v{2} w{3}".format(v_left, v_right, data.linear.x, data.linear.y)
+    v_left  = scale * (data.linear.x - K * data.linear.y)
+    v_right = scale * (data.linear.x + K * data.linear.y)
+    print "v_l {0} v_r {1} v{2} w{3}".format(v_left, v_right, data.linear.x, data.linear.y)
+    vesc.setandmonitorPWM(v_left)
 
 
 def listener():
-    global left_wheel
-    global right_wheel
-
-    left_wheel  = serial.Serial('/dev/ttyACM0')  # open serial port
-    right_wheel = serial.Serial('/dev/ttyACM1')  # open serial port
-
+    global vesc
+    vesc = Vesc()
+    vesc.findandmapcontrollers()
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('OpenRover_Serial', anonymous=True)
-
     rospy.Subscriber('openrover_twist', Twist, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
